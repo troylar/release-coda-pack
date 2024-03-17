5dde8106613a3e9c127f0cbbef749a8f6c05317d
Troy Larson <1931732+troylar@users.noreply.github.com>
2024-03-17 14:54:14 -0400

    refactor: Improve error handling in exec command

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function run() {
   try {
     const packPath = core.getInput("packPath");
     const codaApiToken = core.getInput("codaApiToken");
-    const notes = core.getInput("notes").replace(/\n/g, "\\\n");
+    const notes = core.getInput("notes").replace(/\n/g, "\\n");
 
     if (!codaApiToken) {
       throw new Error("Missing Coda API token");
@@ -21,16 +21,33 @@ async function run() {
     }
     core.info(releasingLogMessage.join(" "));
 
+    let codaOutput = "";
+    let codaError = "";
+    const options = {
+      listeners: {
+        stdout: (data) => {
+          codaOutput += data.toString();
+        },
+        stderr: (data) => {
+          codaError += data.toString();
+        },
+      },
+    };
+
     // handle properly if notes is multiline
-    await exec.exec("npx", [
-      "coda",
-      "release",
-      packPath,
-      "--apiToken",
-      codaApiToken,
-      "--notes",
-      `"${notes}"`,
-    ]);
+    await exec.exec(
+      "npx",
+      [
+        "coda",
+        "release",
+        packPath,
+        "--apiToken",
+        codaApiToken,
+        "--notes",
+        `"${notes}"`,
+      ],
+      options
+    );
     if (codaError) {
       throw new Error(`Coda release failed with error: ${codaError}`);
     }
